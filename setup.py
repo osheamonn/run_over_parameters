d6c053bbf1b533a2b6679b2930be49fefe5ab9eb
Eamonn O'Shea <edo27@cornell.edu>
2021-04-29 12:30:34 -0700

    fix setup

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
    description='Tools for running executables over multiple configurations. ',
    author='Eamonn O\'Shea',
    packages=['parlize'],  #same as name
-   scripts=['bin/parlize_create_multiple_runs']
+    scripts=['bin/parlize_create_multiple_runs', 'bin/parlize_generate_sample_config', 'bin/parlize_run_command']
 )
