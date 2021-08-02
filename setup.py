f47729c57bc8434a7bce0f2e86e45190fb0f958b
Eamonn O''Shea <edo27@cornell.edu>
2021-08-02 18:12:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    name='parlize',
    version='1.0',
-   description='Tools for running executables over multiple configurations. ',
+   description='Tools for generating runs over multiple configurations. ',
    author='Eamonn O\'Shea',
    packages=['parlize'],  #same as name
     scripts=['bin/parlize_create_multiple_runs', 'bin/parlize_generate_sample_config', 'bin/parlize_run_command']
